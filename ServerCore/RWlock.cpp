db3869b70b2f0b6dffa3a2de34c1646fccfa69e8
derekop <derekop2000@gmail.com>
2025-01-25 05:12:50 +0900

    RWLock

--- ServerCore/RWlock.cpp
+++ ServerCore/RWlock.cpp
@@ -3,7 +3,12 @@
 
 void RWlock::WriteLock()
 {
-	uint32 lockThreadId = (_flag & WRITE_FLAG) << 16;
+	uint32 lockThreadId = (_flag & WRITE_FLAG) >> 16;
+	if (LThreadId == lockThreadId)
+	{
+		_writeCnt++;
+		return;
+	}
 	uint32 expected = EMPTY_FLAG;
 	uint32 desired = (LThreadId << 16) & WRITE_FLAG;
 	while (true)
@@ -16,17 +21,39 @@ void RWlock::WriteLock()
 				break;
 			}
 		}
+		this_thread::sleep_for(0s);
 	}
 }
 
 void RWlock::WriteUnLock()
 {
+	if (--_writeCnt == 0)
+	{
+		_flag.store(EMPTY_FLAG);
+	}
 }
 
 void RWlock::ReadLock()
 {
+	uint32 lockThreadId = (_flag & WRITE_FLAG) >> 16;
+	if (LThreadId == lockThreadId)
+	{
+		_flag.fetch_add(1);
+		return;
+	}
+	while (true)
+	{
+		for (int i = 0; i < MAX_SPIN_CNT; i++)
+		{
+			uint32 expected = (_flag.load() & READ_FLAG);
+			if(_flag.compare_exchange_strong(expected, expected + 1))
+				return;
+		}
+		this_thread::sleep_for(0s);
+	}
 }
 
 void RWlock::ReadUnLock()
 {
+	_flag.fetch_add(-1);
 }
